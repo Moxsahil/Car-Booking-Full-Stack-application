7c8fc09d23f8cd3f0efbc5de7582ff5fd1a84124
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2024-12-17 14:44:02 +0530

    minor

--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext , useEffect } from "react";
-import { UserDataContext } from "../context/userContext";
+import { UserDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { use } from "react";
 
