6bcbfb4fad948d4a404630347d80f448213abbcb
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2024-12-31 01:30:47 +0530

    minor

--- Backend/app.js
+++ Backend/app.js
@@ -16,11 +16,7 @@ connectToDb();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ['https://car-booking-flame.vercel.app/'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true 
-}));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
