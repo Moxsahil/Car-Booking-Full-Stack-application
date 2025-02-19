9ee52bd133dcfbc25d5b2bafb0faf046abb7ca15
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2025-02-19 11:56:11 +0530

    fixed

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,11 @@ connectToDb();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
