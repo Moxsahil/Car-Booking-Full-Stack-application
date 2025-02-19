16edc38f1617cc9f2909cd7f6aee534762e6cdc7
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2025-02-19 12:08:17 +0530

    minor

--- Backend/app.js
+++ Backend/app.js
@@ -14,18 +14,28 @@ const paymentRoutes = require("./routes/payment.routes");
 
 connectToDb();
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options("*", cors());
+// app.use(cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+// app.options("*", cors());
 
 app.use(express.json());
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allowed methods
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200); // Handle preflight requests
+    }
+
+    next();
+});
 
 
 app.get("/", (req, res) => {
