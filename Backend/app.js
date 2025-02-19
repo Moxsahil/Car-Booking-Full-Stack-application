8ca563c0858ea2c348d48ab76fa032294318005b
Sahil Barak <94597148+Moxsahil@users.noreply.github.com>
2025-02-19 12:03:36 +0530

    fixed

--- Backend/app.js
+++ Backend/app.js
@@ -14,13 +14,15 @@ const paymentRoutes = require("./routes/payment.routes");
 
 connectToDb();
 
-app.use(express.json());
-
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.options("*", cors());
+
+app.use(express.json());
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
